2487c64f39517432ddbe7afcb2c462692ca799ab
dd4rky <nemodarky@gmail.com>
2026-03-14 19:46:20 +0300

    Project created

---
+++ src/main/java/ru/dd4rky/notificationserver/NotificationServerApplication.java
@@ -0,0 +1,13 @@
+package ru.dd4rky.notificationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationServerApplication.class, args);
+    }
+
+}
