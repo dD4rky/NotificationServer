832d842e49dd2b1be4f1a09fbaabc6808c8549a7
dd4rky <nemodarky@gmail.com>
2026-03-14 20:25:52 +0300

    Created Notification Entity

--- src/main/java/ru/dd4rky/notificationserver/entity/Notification.java
+++ src/main/java/ru/dd4rky/notificationserver/entity/Notification.java
@@ -1,13 +1,35 @@
 package ru.dd4rky.notificationserver.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
+import lombok.Getter;
+import org.aspectj.weaver.ast.Not;
+
+import java.time.LocalDateTime;
 
 @Data
 @Entity
+@Table(name="notifications")
 public class Notification {
-    @Id
+    @Id @GeneratedValue(strategy = GenerationType.UUID)
     private Long id;
+    private LocalDateTime created;
+    @Enumerated(EnumType.ORDINAL)
+    private NotificationStatus status;
 
+    private String initiator;
+    private String message;
+
+    public enum NotificationStatus {
+        UNDEFINED(0),
+        CREATED (1),
+        IN_PROCESS(2),
+        DELIVERED(3);
+
+        @Getter
+        private final int code;
+        NotificationStatus(int code) {
+            this.code = code;
+        }
+    }
 }
